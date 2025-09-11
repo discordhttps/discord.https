772340b16b739e216df55c21d2471bff56f348ca
fourcolverleaf <229131399+fourcolverleaf@users.noreply.github.com>
2025-09-11 07:43:06 +0545

    chore(error): added index.js for easier imports

---
+++ src/structures/errors/index.ts
@@ -0,0 +1,7 @@
+export {
+  DiscordHttpsError,
+  DiscordHttpsTypeError,
+  DiscordHttpsRangeError,
+} from "./DiscordHttpsError.js";
+export { DiscordHttpsErrorCodes } from "./ErrorCode.js";
+export { Messages } from "./ErrorMessage.js";
